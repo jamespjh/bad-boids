5bbd8b96a5c97d2d69e66fb1d5aeebde1c3e334f
James Hetherington <j.hetherington@ucl.ac.uk>
2014-03-06 15:28:22 +0000

    Use floating point comparison

--- test_boids.py
+++ test_boids.py
@@ -1,5 +1,5 @@
 from boids import update_boids
-from nose.tools import assert_equal
+from nose.tools import assert_almost_equal
 import os
 import yaml
 
@@ -7,5 +7,7 @@ def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
     boid_data=regression_data["before"]
     update_boids(boid_data)
-    assert_equal(regression_data["after"],boid_data)
+    for after,before in zip(regression_data["after"],boid_data):
+        for after_value,before_value in zip(after,before): 
+            assert_almost_equal(after_value,before_value,delta=0.01)
 	
